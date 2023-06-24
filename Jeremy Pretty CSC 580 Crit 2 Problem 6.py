d417b2dc5871c869500ea48a9f126f02afb96511
Jeremy D. Pretty <70484698+jpretty01@users.noreply.github.com>
2023-06-24 17:02:33 -0500

    Update
    Update

--- Jeremy Pretty CSC 580 Crit 2 Problem 6.py
+++ Jeremy Pretty CSC 580 Crit 2 Problem 6.py
@@ -1,59 +1,73 @@
-import numpy as np
 import tensorflow as tf
+import numpy as np
 import matplotlib.pyplot as plt
 
-# Set seeds for reproducibility
-np.random.seed(101)
-tf.random.set_seed(101)
+# Disable eager execution
+tf.compat.v1.disable_eager_execution()
 
-# Generating random linear data
-x = np.linspace(0, 50, 50)
-y = np.linspace(0, 50, 50)
+# Import the MNIST dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Adding noise to the random linear data
-x += np.random.uniform(-4, 4, 50)
-y += np.random.uniform(-4, 4, 50)
+# Reshape and normalize the data
+train_images = x_train.reshape(60000, 784).astype('float32') / 255.0
+test_images = x_test.reshape(10000, 784).astype('float32') / 255.0
+train_labels = tf.keras.utils.to_categorical(y_train, 10)
+test_labels = tf.keras.utils.to_categorical(y_test, 10)
 
-n = len(x)  # Number of data points
+# Define the neural network architecture
+input_images = tf.compat.v1.placeholder(tf.float32, shape=[None, 784])
+target_labels = tf.compat.v1.placeholder(tf.float32, shape=[None, 10])
 
-# 1) Plot the training data
-plt.scatter(x, y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Training Data')
-plt.show()
+# Define the neural network model
+hidden_nodes = 512
+input_weights = tf.Variable(tf.random.truncated_normal([784, hidden_nodes]))
+input_biases = tf.Variable(tf.zeros([hidden_nodes]))
+hidden_weights = tf.Variable(tf.random.truncated_normal([hidden_nodes, 10]))
+hidden_biases = tf.Variable(tf.zeros([10]))
 
-# 2) Define the model architecture using Keras API
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(1, input_shape=(1,))
-])
+input_layer = tf.matmul(input_images, input_weights) + input_biases
+hidden_layer = tf.nn.relu(input_layer)
+digit_weights = tf.matmul(hidden_layer, hidden_weights) + hidden_biases
 
-# 3) Define hyperparameters
-learning_rate = 0.001
-training_epochs = 10000
+# Define the loss function and optimizer
+loss_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=digit_weights, labels=target_labels))
 
-# 4) Compile the model with a loss function and optimizer
-model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate),
-              loss='mean_squared_error')
+# Define methods to measure accuracy
+correct_prediction = tf.equal(tf.argmax(digit_weights, 1), tf.argmax(target_labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# 5) Perform data preprocessing
-x_norm = (x - np.mean(x)) / np.std(x)
-y_norm = (y - np.mean(y)) / np.std(y)
+# Create a session
+sess = tf.compat.v1.Session()
 
-# 6) Train the model
-history = model.fit(x_norm, y_norm, epochs=training_epochs, verbose=0)
+# Define different batch sizes
+batch_sizes = [64, 128, 256]  # Example: List of different batch sizes
 
-# 7) Print the results for training loss, weight, and bias
-training_loss = history.history['loss'][-1]
-weight, bias = model.get_weights()
-print("Training loss =", training_loss)
-print("Weight =", weight)
-print("Bias =", bias)
+for batch_size in batch_sizes:
+    # Calculate the number of batches
+    num_batches = len(train_images) // batch_size
 
-# 8) Plot the fitted line on top of the original data
-plt.scatter(x, y)
-plt.plot(x, np.squeeze(weight) * x_norm + bias, 'r')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Fitted Line')
-plt.show()
+    # Define the optimizer with the current batch size
+    optimizer = tf.compat.v1.train.GradientDescentOptimizer(0.5).minimize(loss_function)
+
+    # Initialize variables
+    sess.run(tf.compat.v1.global_variables_initializer())
+
+    # Train the neural network
+    for epoch in range(20):
+        avg_loss = 0.0
+        for batch in range(num_batches):
+            start_idx = batch * batch_size
+            end_idx = (batch + 1) * batch_size
+            batch_X = train_images[start_idx:end_idx]
+            batch_Y = train_labels[start_idx:end_idx]
+            _, loss = sess.run([optimizer, loss_function], feed_dict={input_images: batch_X, target_labels: batch_Y})
+            avg_loss += loss / num_batches
+
+        print("Epoch:", epoch + 1, " Loss:", avg_loss)
+
+    # Calculate and print the accuracy on the test data
+    test_acc = sess.run(accuracy, feed_dict={input_images: test_images, target_labels: test_labels})
+    print("Batch Size:", batch_size, " Accuracy:", test_acc)
+    print()
+
